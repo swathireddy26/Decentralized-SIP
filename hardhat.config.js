1f40017fae5682efd77660059b953518ecfaadb1
swathireddy26 <swathireddyg54@gmail.com>
2022-02-05 18:10:26 +0530

    Scaffolding of the project

---
+++ hardhat.config.js
@@ -0,0 +1,23 @@
+require("@nomiclabs/hardhat-ethers");
+require('dotenv').config({path: './.env'});
+
+module.exports = {
+  defaultNetwork: "matic",
+  networks: {
+    hardhat: {
+    },
+    matic: {
+      url: "https://rpc-mumbai.maticvigil.com",
+      accounts: [process.env.PRIVATE_KEY]
+    }
+  },
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 200
+      }
+    }
+  },
+}
